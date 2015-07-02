b89b4f037ba8e2ec835e894ffe919e2965017243
Marina Manrique <mmanrique@era7.com>
2015-07-02 14:26:11 +0200

    make install for compressinglib bundle

--- src/main/scala/compressinglibs.scala
+++ src/main/scala/compressinglibs.scala
@@ -6,7 +6,8 @@ import ohnosequences.statika._, bundles._, instructions._
 case object compressinglibs extends Bundle() {
 
   def install: Results = {
-    // do someting here
+
+		Seq("yum", "install", "-y", "gzip", "libzip", "zip", "unzip", "zlib", "bzip2", "lzo", "lzop") ->-
     success(s"${bundleName} is installed")
   }
 
